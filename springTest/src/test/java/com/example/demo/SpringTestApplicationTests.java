3cb3e3f0b77f19d63a3d65dddda752a4f4e55af8
91990 <nadeemm3004@gmail.com>
2022-07-13 16:46:31 +0530

    first commit

---
+++ springTest/src/test/java/com/example/demo/SpringTestApplicationTests.java
@@ -0,0 +1,27 @@
+package com.example.demo;
+
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+class SpringTestApplicationTests {
+
+	@Autowired
+	StudentRepo repo;
+	
+	@Test
+	public void test() {
+		System.out.println("Enter");
+		Student s=new Student();
+		s.setName("Thomas");
+		s.setEmail("Pet@gmail.com");
+		assertNotNull(repo.save(s));
+	}
+}
